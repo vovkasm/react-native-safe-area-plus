87964a879be40ce482708303e6d288d20de16a2f
Vladimir Timofeev <vovkasm@gmail.com>
2018-07-19 02:12:59 +0300

    fix tsconfig, and draw green safearea

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const react_1 = tslib_1.__importDefault(require("react"));
-const react_native_1 = require("react-native");
-const VTSafeAreaView = react_native_1.requireNativeComponent('VTSafeAreaView');
+import React from 'react';
+import { requireNativeComponent } from 'react-native';
+const VTSafeAreaView = requireNativeComponent('VTSafeAreaView');
 /**
  * Renders nested content and automatically applies paddings reflect the portion of the view
  * that is not covered by navigation bars, tab bars, toolbars, and other ancestor views.
  * Moreover, and most importantly, Safe Area's paddings reflect physical limitation of the screen,
  * such as rounded corners or camera notches (aka sensor housing area on iPhone X).
  */
-class SafeAreaView extends react_1.default.Component {
+export class SafeAreaView extends React.Component {
     render() {
-        return <VTSafeAreaView {...this.props}/>;
+        return React.createElement(VTSafeAreaView, Object.assign({}, this.props));
     }
 }
-exports.SafeAreaView = SafeAreaView;
--- samples/SafeAreaSample/App.js
+++ samples/SafeAreaSample/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import { SafeAreaView } from 'react-native-safe-area-plus'
+
+const s = StyleSheet.create({
+  content: {
+    backgroundColor: 'rgb(0, 255, 0)',
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+  },
+})
 
 export default class App extends React.Component {
   render() {
-    return (<View><Text>Hello, world</Text></View>)
+    return <SafeAreaView style={s.safeArea}><View style={s.content} /></SafeAreaView>
   }
 }
